7b5072959e2336ebe216514110a07805b0f87ea6
Eddú Meléndez <eddu.melendez@gmail.com>
2019-10-26 11:54:23 -0500

    Add gcp integration

--- src/main/java/com/example/hademo/UserRepository.java
+++ src/main/java/com/example/hademo/UserRepository.java
@@ -1,8 +1,9 @@
 package com.example.hademo;
 
+import org.springframework.cloud.gcp.data.spanner.repository.SpannerRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends SpannerRepository<User, String> {
 
 	User findByUsername(String username);
 }
